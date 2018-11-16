d0c86a65a534342dea156e8fa5e2744cbcca304f
Chenyarong <157655922@qq.com>
2018-11-16 14:34:43 +0800

    依次打印1的阶乘，2的阶乘，3的阶乘...

---
+++ 静态static打印0阶乘/静态static打印0阶乘/static打印阶乘.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fac(int n)
+{
+	static int f = 1;
+	f = f*n;
+	return (f);
+}
+
+int main()
+{
+	int  fac(int n);
+	int i;
+	for (i = 1; i <= 5; i++)
+	{
+		printf("%d!=%d\n", i, fac(i));
+	}
+	system("pause");
+	return 0;
+}
